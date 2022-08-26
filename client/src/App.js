3fcf22c591eedb201b7460485c3c3795b60f406b
tonylf <tonylonefight@gmail.com>
2022-08-26 00:55:46 -0500

    got an image in the webpage. a move forward.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import background from './bgimage.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={background} className="Appbackground" alt="background" height="80%" width="80%"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. TLF
         </p>
         <a
           className="App-link"
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      </div>
   );
 }
 
